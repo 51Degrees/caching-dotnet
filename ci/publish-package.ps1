5d389d3577af656086da939637eb969b4b1c8544
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-12 17:14:30 +0100

    Update publish-package.ps1

--- ci/publish-package.ps1
+++ ci/publish-package.ps1
@@ -2,11 +2,11 @@
 param(
     [string]$ProjectDir = ".",
     [string]$Name = "Release_x64",
-    [Parameter(Mandatory=$true)]
+    #[Parameter(Mandatory=$true)]
     [string]$ApiKey
 )
 
-./dotnet/publish-package-nuget.ps1 -RepoName "caching-dotnet-test" -ProjectDir $ProjectDir -Name $Name -ApiKey $ApiKey
+./dotnet/publish-package-nuget.ps1 -RepoName "caching-dotnet-test" -ProjectDir $ProjectDir -Name $Name #-ApiKey $ApiKey
 
 
 exit $LASTEXITCODE
