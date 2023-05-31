ba059b8a631b26e8f93310da9fb1b8af7ba53d03
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-31 11:23:23 +0100

    Update setup-environment.ps1

--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -8,20 +8,5 @@ param(
     [string]$BuildMethod,
     [hashtable]$Keys
 )
-$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
-
-if ($BuildMethod -ne "dotnet") {
-
-    # Setup the MSBuild environment if it is required.
-    ./environments/setup-msbuild.ps1
-    ./environments/setup-vstest.ps1
-}
-
-if ($IsLinux) {
-    sudo apt-get update
-    # Install multilib, as this may be required.
-    sudo apt-get install -y gcc-multilib g++-multilib
-
-}
-
+Write-Output "Setting SUPER_RESOURCE_KEY environmental variable for '$Name'"
 $env:SUPER_RESOURCE_KEY = $Keys.TestResourceKey
