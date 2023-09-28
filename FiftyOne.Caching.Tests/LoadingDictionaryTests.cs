f1468fac6f6170d4023a1a4792078cf83cbe7e80
Maksym Kucherov <maximus.the0@gmail.com>
2023-09-28 18:04:35 +0300

    Bump delays.

--- FiftyOne.Caching.Tests/LoadingDictionaryTests.cs
+++ FiftyOne.Caching.Tests/LoadingDictionaryTests.cs
@@ -226,9 +226,9 @@ namespace FiftyOne.Caching.Tests
         {
             // Arrange
 
-            const int baseStepMS = 150;
+            const int baseStepMS = 300;
             var value = "teststring";
-            const int GATE_TIMEOUT_MS = 50;
+            const int GATE_TIMEOUT_MS = 120;
             var sourceForLoader = new CancellationTokenSource();
             Func<CancellationToken, CancellationToken> tokenOverride = _ => sourceForLoader.Token;
             var loader = new ReturnKeyLoader<string>(3 * baseStepMS, tokenOverride, tokenOverride);
